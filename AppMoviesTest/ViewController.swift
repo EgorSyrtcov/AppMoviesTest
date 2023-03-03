41f80de89de2b2c4b3d85c668af050354a58e6f6
Egor Syrtcov <sev_84@inbox.ru>
2023-03-04 01:05:51 +0300

    Initial Commit

---
+++ AppMoviesTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMoviesTest
+//
+//  Created by Egor Syrtcov on 4.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
